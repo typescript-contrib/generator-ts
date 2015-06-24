2567030de8c045872686b5c92e292d071925e226
Diullei Gomes <diullei@gmail.com>
2015-06-24 14:01:19 -0300

    fix

--- app/util.ts
+++ app/util.ts
@@ -40,15 +40,19 @@ export function mkdir(dirName: string) {
 }
 
 export function isEmptySync(searchPath: string): boolean {
-  try {
-    var stat = fs.statSync(searchPath);
-  } catch (e) {
-    return true;
-  }
-  if (stat.isDirectory()) {
-    var items = fs.readdirSync(searchPath);
-    return !items || !items.length;
-  }
-  var file = fs.readFileSync(searchPath);
-  return !file || !file.length;
+    try {
+        var stat = fs.statSync(searchPath);
+    } catch (e) {
+        return true;
+    }
+
+    if (stat.isDirectory()) {
+        var items = fs.readdirSync(searchPath);
+        items = items.filter((f) => f != '.git');
+        return !items || !items.length;
+    }
+
+    var file = fs.readFileSync(searchPath);
+
+    return !file || !file.length;
 };
